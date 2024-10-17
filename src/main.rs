47adbdab6f22e7775c4b33bfefb084caa2894a6e
Daniel Pyon <pyondohyun@gmail.com>
2024-10-16 21:38:35 -0700

    read dmesg log

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::Read;
+
+// sudo strace -tt -p <pid>
+// sudo dmesg -w | grep audit
+
+// ./compile.sh
+
+// sudo apparmor -r /etc/apparmor.d/home.debian.Downloads.test
+
+/*
+fn parse_dmesg() {
+
+}
+
+fn parse_strace() {
+
+}
+*/
+
 fn main() {
-    println!("Hello, world!");
+    let dmesg_path = Path::new("dmesg.txt");
+    let display = dmesg_path.display();
+
+    let mut file = match File::open(&dmesg_path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("{} contains\n{}", display, s),
+    }
+
+    // parse_strace(&mut s);
 }
