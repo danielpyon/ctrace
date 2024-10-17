a5b517e9a6ac9d70bccfd82ac128c7db211c53c6
Daniel Pyon <pyondohyun@gmail.com>
2024-10-16 22:49:40 -0700

    find audit log entries

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::io::Read;
 // adjustable interval
 // colorful output
 
+#[derive(Default)]
 struct AuditLogEntry {
     cap: String,
     time: f64
@@ -17,11 +18,43 @@ struct AuditLogEntry {
 
 fn parse_dmesg(s: &str) {
     // store ordered list of structs, each containing which capability was triggered (eg: net_raw)
-    let v: Vec<AuditLogEntry> = Vec::new();
+    let mut audit_log_entries: Vec<AuditLogEntry> = Vec::new();
 
     for line in s.lines() {
-        println!("{}", line);
         // TODO: parse the data and add it to v
+        let time_str = line.find("audit(")
+            .and_then(|start| {
+                let after_audit = &line[start+6..];
+                after_audit.find(':')
+                    .map(|end| &after_audit[..end])
+            });
+        
+        let mut entry = AuditLogEntry::default();
+
+        entry.time = match time_str {
+            Some(t) => {
+                match t.parse() {
+                    Ok(val) => val,
+                    Err(_) => {
+                        println!("failed to parse {} to double", s);
+                        0.0
+                    }
+                }
+            },
+            None => {
+                println!("failed to find unix timestamp in audit log");
+                0.0
+            },
+        };
+
+        entry.cap = line.find("capname=\"")
+            .and_then(|start| {
+                line[start+9..].find("\"")
+                    .map(|end| &line[start+9..][..end])
+            }).unwrap_or("").to_string();
+
+        println!("found audit log entry (cap={}, time={})", entry.cap, entry.time);
+        audit_log_entries.push(entry);
     }
 }
 
